1ea413f85a8f6004e3313f546e08c93ae1688f95
Josep <pujol.rabat@gmail.com>
2018-11-20 16:10:44 +0000

    added html rendering and templates

--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
  
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello Flask!'
-    
+    return render_template('index.html')
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
